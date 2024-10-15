447a19c3e3fe586fd3eee44fbaeb54ead1b63ddd
ryuken73 <ryuken73@gmail.com>
2024-10-15 22:51:41 +0900

    update: style header title

--- src/Components/HeadTitle.js
+++ src/Components/HeadTitle.js
@@ -12,16 +12,22 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100%;
+  width: 70%;
   
+`
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  width: 100%;
 `
 const Title = styled.div`
   font-size: 40px;
   font-weight: bold;
   /* background: rgba(15, 23, 42, 0.8); */
   padding: 10px;
-  width: 70%;
   border-radius: 10px;
+  width: 100%;
 `
 const BarContainer = styled.div`
   display: flex;
@@ -29,7 +35,7 @@ const BarContainer = styled.div`
   align-items: center;
   color: white;
   z-index: 10;
-  min-width: 90%;
+  width: 100%;
 `
 
 function HeadTitle(props) {
@@ -37,15 +43,21 @@ function HeadTitle(props) {
   
   return (
     <Container>
-      <Title>{activeYear} 미국 대선</Title>
+      <TitleContainer>
+        {activeYear === 2024 && (
+          <div>DEM:{DEM_count}</div>
+        )}
+        <Title>{activeYear} 미국 대선</Title>
+        {activeYear === 2024 && (
+          <div>REP:{REP_count}</div>
+        )}
+      </TitleContainer>
       {activeYear === 2024 &&(
         <BarContainer>
-          <div>DEM:{DEM_count}</div>
           <CountBar
             DEM_count={DEM_count}
             REP_count={REP_count}
           ></CountBar>
-          <div>REP:{REP_count}</div>
         </BarContainer>
       )}
     </Container>
